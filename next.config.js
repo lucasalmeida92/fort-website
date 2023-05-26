83759344791fc28a04a96b36e257f19942611c66
Lucas Almeida <lucasalmeida70@gmail.com>
2023-05-26 03:42:24 +0000

    remove static export mode

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  trailingSlash: true,
+  // trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
 }
