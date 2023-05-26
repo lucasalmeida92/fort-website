35b40e459eaf6b5d8b0d7350f313d2883eb49a1c
Lucas Almeida Siqueira <lucasalmeida70@gmail.com>
2023-05-26 02:02:39 -0300

    WIP: Sponsors section

--- src/app/(home)/TeamsSection/index.tsx
+++ src/app/(home)/TeamsSection/index.tsx
@@ -7,48 +7,46 @@ import './styles.css';
 
 export function TeamsSection() {
   return (
-    <div className="min-h-[50vh] py-8">
-      <Container className="flex flex-col items-center content-center">
-        <h2 className="text-3xl mb-8 uppercase font-bold">Nossos times</h2>
-        <ul className="flex gap-6 flex-wrap justify-center">
-          <li>
-            <Link
-              href="/teams/valorant-male"
-              title="Valorant Masculino"
-              className="p-4 rounded-2xl shadow-lg hover:shadow-2xl hover:-translate-y-1 team"
-            >
-              <h3 className="text-lg">Valorant Masculino</h3>
-              <figure className="bg-white p-2 rounded-xl">
-                <Image src={valorantLogo} alt="Logo Valorant" className="game-logo" />
-              </figure>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/teams/valorant-male"
-              title="Valorant Feminino"
-              className="p-4 rounded-2xl shadow-lg hover:shadow-2xl hover:-translate-y-1 team"
-            >
-              <h3 className="text-lg">Valorant Feminino</h3>
-              <figure className="bg-white p-2 rounded-xl">
-                <Image src={valorantLogo} alt="Logo Valorant" className="game-logo" />
-              </figure>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/teams/valorant-male"
-              title="CS:GO Masculino"
-              className="p-4 rounded-2xl shadow-lg hover:shadow-2xl hover:-translate-y-1 team"
-            >
-              <h3 className="text-lg">CS:GO Masculino</h3>
-              <figure className="bg-white p-2 rounded-xl">
-                <Image src={csGoLogo} alt="Logo CS:GO" className="game-logo" />
-              </figure>
-            </Link>
-          </li>
-        </ul>
-      </Container>
-    </div>
+    <Container className="flex flex-col min-h-[70vh] py-8 items-center justify-center">
+      <h2 className="text-4xl mb-12 uppercase font-bold">Nossos times</h2>
+      <ul className="flex gap-6 flex-wrap justify-center">
+        <li>
+          <Link
+            href="/teams/valorant-male"
+            title="Valorant Masculino"
+            className="p-4 rounded-2xl shadow-lg hover:shadow-2xl hover:-translate-y-1 team"
+          >
+            <h3 className="text-lg">Valorant Masculino</h3>
+            <figure className="bg-white p-2 rounded-xl">
+              <Image src={valorantLogo} alt="Logo Valorant" className="game-logo" />
+            </figure>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/teams/valorant-male"
+            title="Valorant Feminino"
+            className="p-4 rounded-2xl shadow-lg hover:shadow-2xl hover:-translate-y-1 team"
+          >
+            <h3 className="text-lg">Valorant Feminino</h3>
+            <figure className="bg-white p-2 rounded-xl">
+              <Image src={valorantLogo} alt="Logo Valorant" className="game-logo" />
+            </figure>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/teams/valorant-male"
+            title="CS:GO Masculino"
+            className="p-4 rounded-2xl shadow-lg hover:shadow-2xl hover:-translate-y-1 team"
+          >
+            <h3 className="text-lg">CS:GO Masculino</h3>
+            <figure className="bg-white p-2 rounded-xl">
+              <Image src={csGoLogo} alt="Logo CS:GO" className="game-logo" />
+            </figure>
+          </Link>
+        </li>
+      </ul>
+    </Container>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Container } from "@/components/Container";
 import { Hero } from "./Hero";
 import { FeaturedSection } from "./FeaturedSection";
@@ -9,6 +10,34 @@ export default function HomePage() {
       <Hero />
       <FeaturedSection />
       <TeamsSection />
+      <Container className="flex flex-col items-center justify-center min-h-[50vh] py-8">
+        {/* <h2 className="text-4xl mb-12 uppercase font-bold">Quem acredita na fort</h2> */}
+        <div className="carousel rounded-box h-[240px] bg-black bg-opacity-5">
+          <div className="carousel-item mx-4">
+            <Image src="https://www.exitlag.com/img/exitlag.svg" alt="Exitlag" width={220} height={200}  />
+          </div>
+          <div className="carousel-item mx-4">
+            <Image src="https://www.exitlag.com/img/exitlag.svg" alt="Exitlag" width={220} height={200}  />
+          </div>
+          <div className="carousel-item mx-4">
+            <Image src="https://www.exitlag.com/img/exitlag.svg" alt="Exitlag" width={220} height={200}  />
+          </div>
+          <div className="carousel-item mx-4">
+            <Image src="https://www.exitlag.com/img/exitlag.svg" alt="Exitlag" width={220} height={200}  />
+          </div>
+          <div className="carousel-item mx-4">
+            <Image src="https://www.exitlag.com/img/exitlag.svg" alt="Exitlag" width={220} height={200}  />
+          </div>
+          <div className="carousel-item mx-4">
+            <Image src="https://www.exitlag.com/img/exitlag.svg" alt="Exitlag" width={220} height={200}  />
+          </div>
+        </div>
+      </Container>
+      <Container className="flex items-center justify-center min-h-[25vh]">
+        <span className="text-3xl sm:text-5xl md:text-6xl font-bold drop-shadow-xl">
+          <em className="text-pink">#</em>JuntosSomosMaisFort
+        </span>
+      </Container>
     </>
   )
 }
